7a96fec749e175ae2919dd37cb3085d2eb8a7500
JaneWB <brownjannick@att.net>
2018-10-05 23:31:16 -0500

    Week 13-1 Number Cruncher

--- CG _1_13/Program.cs
+++ CG _1_13/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using NumberUtility;
 
 namespace CG__1_13
 {
@@ -6,7 +9,21 @@ namespace CG__1_13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> nums = new List<int>();
+            nums.Add(10);
+            nums.Add(5);
+            nums.Add(6);
+            nums.Add(7);
+            nums.Add(8);
+            nums.Add(2);
+            nums.Add(14);
+            nums.Add(3);
+            nums.Add(4);
+            nums.Add(22);
+
+            Console.WriteLine(NumberCruncher.SumOfEvens(nums));
+
+            Console.ReadLine();
         }
     }
 }
